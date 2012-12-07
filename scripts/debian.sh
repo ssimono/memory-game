775d079b31f3666f713f81c31562fd087160d390
Simon <simon@sa-web.fr>
2012-12-08 00:36:04 +0100

    Add a script to automate deb package generation
    Simply run scripts/debian.sh to get a .deb file

---
+++ scripts/debian.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+
+#
+# This script will package the program into a .deb file
+#
+
+name='memory-game'
+tmp="/tmp/$name"
+base_dir=$(dirname $0)/..
+
+# Make sure the built binary exists
+if [ ! -e $base_dir/$name ]; then
+    echo 'Program memory-game not found. Make sure you first built it'
+    exit
+fi
+
+# Create a temporary directory to build the package
+if [ -e $tmp ]; then
+    rm -r $tmp
+fi
+
+mkdir $tmp
+
+# Create DEBIAN folder to store meta-information about the packge
+mkdir $tmp/DEBIAN
+
+# Write control file
+echo "\
+Package: $name
+Version: 1.0
+Section: base
+Priority: optional
+Architecture: all
+Depends: libsdl-ttf2.0-0,libsdl1.2debian
+Maintainer: Simon Alfassa <simon@sa-web.fr>
+Description: A funny game to train your memory skills"\
+>> $tmp/DEBIAN/control
+
+# Fill with required files
+install -D $base_dir/$name $tmp/usr/bin/$name
+
+cd $base_dir/share
+for i in `find . -type f`
+do
+    install -D $i $tmp/usr/share/$name/$i --mode=644
+done
+
+# Build the debian package
+dpkg-deb --build $tmp
+
+echo 'To install your game:'
+echo 'sudo dpkg -i /tmp/memory-game.deb'
+
+# Remove temp directory
+rm -r $tmp
